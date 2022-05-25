5439443f1d38526f2857a2af8696bb53a52d5fbc
fauzanmzlm <fauzanmazlam88@gmail.com>
2022-05-25 19:08:23 +0800

    commit

---
+++ src/components/ResultsComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <p>Reaction Time : {{ score }}</p>
+    <p class="rank">Rank : {{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 200) {
+            this.rank = 'Ninja Fingers'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail Pace...'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
